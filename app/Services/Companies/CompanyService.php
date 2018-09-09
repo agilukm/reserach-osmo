150b8c116e890c3937da98f62ceae545006cc56b
Agi Lukman <agilukm07@gmail.com>
2018-09-10 05:47:04 +0700

    company by filter

--- app/Services/Companies/CompanyService.php
+++ app/Services/Companies/CompanyService.php
@@ -76,6 +76,20 @@ class CompanyService
 
     public function getNeedReport()
     {
+        $kotas = array();
+        if (\Auth::check()) {
+
+            if (\Auth::user()->roles != 'admin') {
+
+                $accessess = \DB::table('accesses')->where('user_id', \Auth::user()->id)->get();
+                foreach ($accessess as $key => $access) {
+                    $kotas[] = $access->kota;
+                }
+
+                return return \DB::select('select *, datediff(NOW(),last_updated_time) from companies left join pembangkit on pembangkit.company_id = companies.id where exists(select * from pembangkit where pembangkit.company_id = companies.id) and companies.kota in('$kotas')  and (datediff(NOW(), last_updated_time) > 180 OR not exists (select * from laporan where laporan.pembangkit_id = pembangkit.id)) group by companies.id');
+            }
+        }
+
         return \DB::select('select *, datediff(NOW(),last_updated_time) from companies left join pembangkit on pembangkit.company_id = companies.id where exists(select * from pembangkit where pembangkit.company_id = companies.id) and (datediff(NOW(), last_updated_time) > 180 OR not exists (select * from laporan where laporan.pembangkit_id = pembangkit.id)) group by companies.id');
     }
 
